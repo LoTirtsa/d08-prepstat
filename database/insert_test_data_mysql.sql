39788ba73a752ff2c0312c665ab3101e97f81592
Kavantsaari Christoffer <a1800632@myy.haaga-helia.fi>
2018-11-20 09:30:44 +0200

    projektin mallipohjan luonti

---
+++ database/insert_test_data_mysql.sql
@@ -0,0 +1,12 @@
+-- MUISTA! console encoding latin1
+
+INSERT INTO henkilo
+	(etunimi, sukunimi)
+values
+	('Matti', 'Meikäläinen'),
+	('Mikko', 'Mallikas'),
+	('Outi', 'Opiskelija'),
+	('Tiina', 'Tikolainen');
+	
+	
+	
